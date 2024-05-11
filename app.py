627a49a737eca4096a287638a8b7829fa0674feb
nkw1234 <ngkinwai5@gmail.com>
2024-05-11 18:56:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='template')
 
-app = Flask(__name__)
-app.secret_key = "manbearpig_MUDMAN888"
+@app.route('/<user>')
+def hi(user):
+    return render_template('index.html', name=user)
 
-@app.route("/hello")
-def index():
-	flash("what's your name?")
-	return render_template("index.html")
-
-@app.route("/greet", methods=['POST', 'GET'])
-def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+app.run(debug=True)
